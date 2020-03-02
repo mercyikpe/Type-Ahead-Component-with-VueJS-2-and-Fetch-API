89e846f12f83af8729f41cd6af0adccbdeb700a5
mercy ikpe <ikpemercy1@gmail.com>
2020-03-03 00:29:32 +0100

    files for typehead

---
+++ vue-typeahead-component/README.md
@@ -0,0 +1,21 @@
+# vue-typeahead-component
+
+> In this tutorial we are going to build simple VueJS 2 Typeahead Component.We won’t use any libraries such as Axios, jQuery or VueResource - instead we are going to use Fetch API, that’s built into the browser.Usage of Fetch API is pretty straightforward.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ vue-typeahead-component/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<div id="app">
+		<div class="Wrap text-center">
+			<h1>Vue Typeahead</h1>
+			<p>Simple VueJS 2 TypeAhead component built with Fetch Browser API.</p>
+
+			<!-- Our component-->
+			<typeahead
+				source="https://jsonplaceholder.typicode.com/posts"
+				placeholder="What TV Series you are looking for..."
+				filter-key="title"
+				:start-at="2"
+			>
+			</typeahead>
+		</div>
+	</div>
+</template>
+
+<script>
+
+	import Typeahead from './components/Typeahead.vue'
+
+	export default {
+
+		name: 'app',
+
+		components: { Typeahead  }
+
+	}
+
+</script>
+
+<style>
+	body {
+		margin: 0; padding: 0;
+		font-family: 'Open Sans', Arial, sans-serif;
+		box-sizing: border-box;
+		background: linear-gradient(135deg, #41B883 0%,#354953 100%);
+		background-size: cover;
+		min-height: 100vh;
+		color: #fff;
+	}
+	h1 {
+		font-size: 6vw;
+	}
+	h1, h2, h3, h4 {
+		margin: 0; padding: 0;
+		font-family: 'Lobster', Arial, sans-serif;
+	}
+	.text-center {
+		text-align: center;
+	}
+	.Wrap {
+		max-width: 60vw;
+		margin: 15vh auto;
+	}
+</style>
